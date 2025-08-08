55e0d29b7e54ac8e4f9ff35840860d4d66c6b9ce
Anjishnu Banerjee <inviktor27@gmail.com>
2025-08-08 11:20:48 +0530

    Add softbuffer frontend

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,6 @@
-use pixels::{Pixels, SurfaceTexture};
-use std::sync::Arc;
+use softbuffer::Surface;
+use std::num::NonZeroU32;
+use std::rc::Rc;
 use std::time::Instant;
 use winit::dpi::LogicalSize;
 use winit::error::EventLoopError;
@@ -9,18 +10,18 @@ use winit::{application::ApplicationHandler, event::WindowEvent};
 
 const WINDOW_SIZE: LogicalSize<u32> = LogicalSize::new(640, 480);
 
-struct AppState<'a> {
-    window: Arc<Window>,
-    pixels: Pixels<'a>,
+struct AppState {
+    window: Rc<Window>,
+    draw_surface: Surface<Rc<Window>, Rc<Window>>,
     last_frame: Instant,
 }
 
 #[derive(Default)]
-struct App<'a> {
-    state: Option<AppState<'a>>,
+struct App {
+    state: Option<AppState>,
 }
 
-impl ApplicationHandler for App<'_> {
+impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
         if self.state.is_some() {
             eprintln!("Resume called after window initialization.");
@@ -29,16 +30,13 @@ impl ApplicationHandler for App<'_> {
         let win_attr = Window::default_attributes()
             .with_title("StarPSX")
             .with_inner_size(WINDOW_SIZE);
-        let window = Arc::new(event_loop.create_window(win_attr).unwrap());
-        let surface_texture =
-            SurfaceTexture::new(WINDOW_SIZE.width, WINDOW_SIZE.height, window.clone());
-        let mut pixels =
-            Pixels::new(WINDOW_SIZE.width, WINDOW_SIZE.height, surface_texture).unwrap();
-        pixels.enable_vsync(false);
+        let window = Rc::new(event_loop.create_window(win_attr).unwrap());
+        let context = softbuffer::Context::new(window.clone()).unwrap();
+        let draw_surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
 
         self.state = Some(AppState {
-            pixels,
             window,
+            draw_surface,
             last_frame: Instant::now(),
         })
     }
@@ -56,12 +54,16 @@ impl ApplicationHandler for App<'_> {
 
         match event {
             WindowEvent::RedrawRequested => {
-                draw(state.pixels.frame_mut());
-                if let Err(err) = state.pixels.render() {
-                    eprintln!("Pixels could not render: {err}");
-                    event_loop.exit();
-                    return;
-                }
+                state
+                    .draw_surface
+                    .resize(
+                        NonZeroU32::new(WINDOW_SIZE.width).unwrap(),
+                        NonZeroU32::new(WINDOW_SIZE.height).unwrap(),
+                    )
+                    .unwrap();
+                let mut buffer = state.draw_surface.buffer_mut().unwrap();
+                draw(&mut buffer);
+                buffer.present().unwrap();
 
                 let now = Instant::now();
                 let delta = now - state.last_frame;
@@ -76,25 +78,23 @@ impl ApplicationHandler for App<'_> {
     }
 }
 
-fn draw(buffer: &mut [u8]) {
+fn draw(buffer: &mut [u32]) {
     let width = 640;
     let height = 480;
 
-    assert_eq!(buffer.len(), width * height * 4);
+    assert_eq!(buffer.len(), width * height);
 
     for y in 0..height {
         for x in 0..width {
-            let i = (y * width + x) * 4;
+            let i = y * width + x;
 
-            let red = ((x as f32 / width as f32) * 255.0) as u8;
-            let green = ((y as f32 / height as f32) * 255.0) as u8;
-            let blue = 0;
-            let alpha = 255;
+            let red = ((x as f32 / width as f32) * 255.0) as u32;
+            let green = ((y as f32 / height as f32) * 255.0) as u32;
+            let blue = 0u32;
 
-            buffer[i] = red;
-            buffer[i + 1] = green;
-            buffer[i + 2] = blue;
-            buffer[i + 3] = alpha;
+            let color = (red << 16) | (green << 8) | blue;
+
+            buffer[i] = color;
         }
     }
 }
