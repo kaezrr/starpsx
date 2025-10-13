00bbe3ee9b447756d1e0258a288b1598eec422a2
Anjishnu Banerjee <inviktor27@gmail.com>
2025-10-13 19:23:33 +0530

    Cdrom version read

--- core/src/cdrom/mod.rs
+++ core/src/cdrom/mod.rs
@@ -1,3 +1,5 @@
+use arrayvec::ArrayVec;
+
 use crate::{System, mem::ByteAddressable};
 
 pub const PADDR_START: u32 = 0x1F801800;
@@ -10,7 +12,7 @@ bitfield::bitfield! {
     adpcm_busy, _ : 2;
     param_empty, set_param_empty : 3;
     param_write_ready, set_param_write_ready : 4;
-    result_read_ready, _ : 5;
+    result_read_ready, set_result_read_ready : 5;
     data_request, _ : 6;
     busy, _ : 7;
 }
@@ -29,7 +31,7 @@ bitfield::bitfield! {
 bitfield::bitfield! {
     #[derive(Default)]
     struct Hintsts(u8);
-    interrupt_hc05, _ : 2, 0;
+    interrupt, set_interrupt : 2, 0;
     sound_map_empty, _ : 3;
     sound_map_ready, _ : 4;
 }
@@ -46,6 +48,8 @@ pub struct CdRom {
     address: Address,
     hintsts: Hintsts,
     hintmsk: Hintmsk,
+    parameters: ArrayVec<u8, 16>,
+    results: ArrayVec<u8, 16>,
 }
 
 impl Default for CdRom {
@@ -53,10 +57,14 @@ impl Default for CdRom {
         let address = Address(0x18);
         let hintsts = Hintsts::default();
         let hintmsk = Hintmsk::default();
+        let parameters = ArrayVec::default();
+        let results = ArrayVec::default();
         Self {
             address,
             hintsts,
             hintmsk,
+            parameters,
+            results,
         }
     }
 }
@@ -67,7 +75,7 @@ impl CdRom {
         self.address.0 = (self.address.0 & !3) | (val & 3);
     }
 
-    fn read_addr(&mut self) -> u8 {
+    fn read_addr(&self) -> u8 {
         self.address.0
     }
 
@@ -78,6 +86,48 @@ impl CdRom {
     fn write_hintmsk(&mut self, val: u8) {
         self.hintmsk.0 = val;
     }
+
+    fn push_parameter(&mut self, val: u8) {
+        if self.parameters.is_empty() {
+            self.address.set_param_empty(false);
+        }
+
+        self.parameters.push(val);
+
+        if self.parameters.is_full() {
+            self.address.set_param_write_ready(false);
+        }
+    }
+
+    fn test(&mut self, cmd: u8) -> Response {
+        match cmd {
+            // CDROM Version
+            0x20 => Response::INT3([0x94, 0x09, 0x19, 0xC0].into()),
+            _ => unimplemented!("cdrom command Test {cmd:02x}"),
+        }
+    }
+}
+
+fn exec_command(system: &mut System, cmd: u8) {
+    let cdrom = &mut system.cdrom;
+    let response = match cmd {
+        0x19 => cdrom.test(cdrom.parameters[0]),
+        _ => unimplemented!("cdrom command {cmd:02x}"),
+    };
+
+    match response {
+        Response::INT3(array_vec) => {
+            cdrom.results.clear();
+            cdrom.results.extend(array_vec);
+            cdrom.hintsts.set_interrupt(3);
+        }
+    }
+
+    cdrom.parameters.clear();
+    cdrom.address.set_param_empty(true);
+    cdrom.address.set_param_write_ready(true);
+    cdrom.address.set_result_read_ready(true);
+    system.irqctl.stat().set_cdrom(true);
 }
 
 pub fn read<T: ByteAddressable>(system: &mut System, addr: u32) -> T {
@@ -99,8 +149,14 @@ pub fn write<T: ByteAddressable>(system: &mut System, addr: u32, data: T) {
 
     match (cdrom.address.bank(), offs) {
         (_, 0) => cdrom.write_addr(val),
+        (0, 1) => exec_command(system, val),
+        (0, 2) => cdrom.push_parameter(val),
         (1, 3) => cdrom.write_hclrctl(val),
         (1, 2) => cdrom.write_hintmsk(val),
         (x, y) => unimplemented!("cdrom write bank {x} reg {y} <- {data:08x}"),
     }
+}
+
+enum Response {
+    INT3(ArrayVec<u8, 4>),
 }
