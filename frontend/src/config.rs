5ada7ea098a203584df75279ba48b941d1ad4e6a
Anjishnu Banerjee <inviktor27@gmail.com>
2026-03-08 23:48:10 +0530

    Add full speed toggle in ui and cli

--- frontend/src/config.rs
+++ frontend/src/config.rs
@@ -51,6 +51,10 @@ pub struct Args {
     #[arg(short, long)]
     debugger_view: bool,
 
+    /// Run emulator at full speed
+    #[arg(short, long)]
+    full_speed: bool,
+
     /// File to start the emulator with
     #[arg(value_name = "FILE")]
     file: Option<PathBuf>,
@@ -61,6 +65,7 @@ pub struct LaunchConfig {
     pub runnable_path: Option<PathBuf>,
     pub auto_run: bool,
     pub config_path: PathBuf,
+    pub full_speed: bool,
 }
 
 impl LaunchConfig {
@@ -87,8 +92,9 @@ impl LaunchConfig {
         Ok(Self {
             app_config,
             runnable_path,
-            auto_run: args.auto_run,
             config_path,
+            auto_run: args.auto_run,
+            full_speed: args.full_speed,
         })
     }
 }
