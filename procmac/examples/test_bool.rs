d17e2057fc6db068dc3e636e876ccb3467ea5bf0
Anjishnu Banerjee <inviktor27@gmail.com>
2026-03-28 11:31:03 +0530

    Add boolable proc macro crate

---
+++ procmac/examples/test_bool.rs
@@ -0,0 +1,31 @@
+use procmac::Boolable;
+
+#[derive(Debug, PartialEq, Boolable)]
+enum Switch {
+    Off = 0,
+    On = 1,
+}
+
+#[derive(Debug, PartialEq, Boolable)]
+enum Integrity {
+    Corrupt = 0,
+    Valid = 1,
+}
+
+fn main() {
+    // Test: bool -> Enum
+    let s_on = Switch::from(true);
+    let s_off = Switch::from(false);
+
+    assert_eq!(s_on, Switch::On);
+    assert_eq!(s_off, Switch::Off);
+
+    // Test: Enum -> bool
+    let b_true: bool = Integrity::Valid.into();
+    let b_false: bool = Integrity::Corrupt.into();
+
+    assert!(b_true);
+    assert!(!b_false);
+
+    println!("All tests passed! Your macro is working.");
+}
